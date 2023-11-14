a06b83a3248f58a9b6169af94b7911f3d7cbcd85
ikecabrido <russcabz28@gmail.com>
2023-11-14 16:24:14 +0800

    testing routes

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\BusResource;
+use App\Models\Bus;
 
 class BusController extends Controller
 {
     public function index() {
-        // return BusResource::collection(Bus::all());
-        return 'Successful route! You have a connection';
+        return BusResource::collection(Bus::all());
+        // return 'Successful route! You have a connection';
     }
 
     public function getBusSchedule() {}
